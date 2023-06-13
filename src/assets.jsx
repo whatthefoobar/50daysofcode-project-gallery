1fc54b6c846d686d2544a3f12d0c92b3cf32b461
whatthefoobar <irina.mntn@gmail.com>
2023-06-13 11:18:12 +0200

    try again netlify

---
+++ src/assets.jsx
@@ -0,0 +1,89 @@
+import p1 from "./img/1.png";
+import p2 from "./img/2.png";
+import p3 from "./img/3.png";
+import p4 from "./img/4.png";
+import p5 from "./img/5.png";
+import p6 from "./img/6.png";
+import p7 from "./img/7.png";
+import p8 from "./img/8.png";
+import p9 from "./img/9.png";
+import p10 from "./img/10.png";
+import p11 from "./img/11.png";
+import p12 from "./img/12.png";
+import p13 from "./img/13.png";
+import p14 from "./img/14.png";
+import p15 from "./img/15.png";
+import p16 from "./img/16.png";
+import p17 from "./img/17.png";
+import p18 from "./img/18.png";
+import p19 from "./img/19.png";
+import p20 from "./img/20.png";
+import p21 from "./img/21.png";
+import p22 from "./img/22.png";
+import p23 from "./img/23.png";
+import p24 from "./img/24.png";
+import p25 from "./img/25.png";
+import p26 from "./img/26.png";
+import p27 from "./img/27.png";
+import p28 from "./img/28.png";
+import p29 from "./img/29.png";
+import p30 from "./img/30.png";
+import p31 from "./img/31.png";
+import p32 from "./img/32.png";
+import p33 from "./img/33.png";
+import p34 from "./img/34.png";
+import p35 from "./img/35.png";
+import p36 from "./img/36.png";
+import p37 from "./img/37.png";
+import p38 from "./img/38.png";
+import p39 from "./img/39.png";
+import p40 from "./img/40.png";
+import p41 from "./img/41.png";
+import p42 from "./img/42.png";
+
+const assets = {
+  p1,
+  p2,
+  p3,
+  p4,
+  p5,
+  p6,
+  p7,
+  p8,
+  p9,
+  p10,
+  p11,
+  p12,
+  p13,
+  p14,
+  p15,
+  p16,
+  p17,
+  p18,
+  p19,
+  p20,
+  p21,
+  p22,
+  p23,
+  p24,
+  p25,
+  p26,
+  p27,
+  p28,
+  p29,
+  p30,
+  p31,
+  p32,
+  p33,
+  p34,
+  p35,
+  p36,
+  p37,
+  p38,
+  p39,
+  p40,
+  p41,
+  p42,
+};
+
+export default assets;
