af99acedfc03f77c66984299272f9dd9cd29cc27
whatthefoobar <irina.mntn@gmail.com>
2023-06-13 11:12:07 +0200

    try deply netlify again

---
+++ src/asstets.jsx
@@ -0,0 +1,89 @@
+import p1 from "./assets/img/1.png";
+import p2 from "./assets/img/2.png";
+import p3 from "./assets/img/3.png";
+import p4 from "./assets/img/4.png";
+import p5 from "./assets/img/5.png";
+import p6 from "./assets/img/6.png";
+import p7 from "./assets/img/7.png";
+import p8 from "./assets/img/8.png";
+import p9 from "./assets/img/9.png";
+import p10 from "./assets/img/10.png";
+import p11 from "./assets/img/11.png";
+import p12 from "./assets/img/12.png";
+import p13 from "./assets/img/13.png";
+import p14 from "./assets/img/14.png";
+import p15 from "./assets/img/15.png";
+import p16 from "./assets/img/16.png";
+import p17 from "./assets/img/17.png";
+import p18 from "./assets/img/18.png";
+import p19 from "./assets/img/19.png";
+import p20 from "./assets/img/20.png";
+import p21 from "./assets/img/21.png";
+import p22 from "./assets/img/22.png";
+import p23 from "./assets/img/23.png";
+import p24 from "./assets/img/24.png";
+import p25 from "./assets/img/25.png";
+import p26 from "./assets/img/26.png";
+import p27 from "./assets/img/27.png";
+import p28 from "./assets/img/28.png";
+import p29 from "./assets/img/29.png";
+import p30 from "./assets/img/30.png";
+import p31 from "./assets/img/31.png";
+import p32 from "./assets/img/32.png";
+import p33 from "./assets/img/33.png";
+import p34 from "./assets/img/34.png";
+import p35 from "./assets/img/35.png";
+import p36 from "./assets/img/36.png";
+import p37 from "./assets/img/37.png";
+import p38 from "./assets/img/38.png";
+import p39 from "./assets/img/39.png";
+import p40 from "./assets/img/40.png";
+import p41 from "./assets/img/41.png";
+import p42 from "./assets/img/42.png";
+
+const assets = {
+  p1,
+  p2,
+  p3,
+  p4,
+  p5,
+  p6,
+  p7,
+  p8,
+  p9,
+  p10,
+  p11,
+  p12,
+  p13,
+  p14,
+  p15,
+  p16,
+  p17,
+  p18,
+  p19,
+  p20,
+  p21,
+  p22,
+  p23,
+  p24,
+  p25,
+  p26,
+  p27,
+  p28,
+  p29,
+  p30,
+  p31,
+  p32,
+  p33,
+  p34,
+  p35,
+  p36,
+  p37,
+  p38,
+  p39,
+  p40,
+  p41,
+  p42,
+};
+
+export default assets;
